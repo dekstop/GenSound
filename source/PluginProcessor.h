927c0c8d1a32f3b5b4ae41f0cb5aa44604cd6801
Martin Dittus <martin@dekstop.de>
2026-06-24 21:51:50 +0100

    build fixes

--- source/PluginProcessor.h
+++ source/PluginProcessor.h
@@ -1,6 +1,9 @@
 #pragma once
 
-#include <JuceHeader.h>
+#include <juce_audio_processors/juce_audio_processors.h>
+#include <juce_audio_utils/juce_audio_utils.h>
+#include <juce_gui_basics/juce_gui_basics.h>
+
 #include "audio/VoiceManager.h"
 #include "compile/ScriptManager.h"
 #include "compile/FileWatcher.h"
